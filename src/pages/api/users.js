d0b4f1bf02487f7140f9c2b455405a875c57d1df
Nishant Kumar <nishants440@gmail.com>
2023-04-12 13:53:06 +0530

    PUT Method

--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -1,22 +1,30 @@
-let data = [
-  {
-    name: "Nishant",
-    age: 26,
-  },
-  {
-    name: "Uday",
-    age: 36,
-  },
-  {
-    name: "Sanjeet",
-    age: 46,
-  },
-];
+let data = [];
 
 export default function users(req, res) {
   if (req.method === "GET") {
     res.status(200).json(data);
   } else if (req.method === "POST") {
+    let id = req.body.id;
+    let name = req.body.name;
+    let age = req.body.age;
+
+    let user = {
+      id: id,
+      name: name,
+      age: age,
+    };
+    data.push(user);
+    res.status(201).json(user);
+  } else if (req.method === "DELETE") {
+    let id = req.body.id;
+
+    let indexToDelete = data.findIndex((user) => user.id === id);
+    data.splice(indexToDelete, 1);
+
+    let userToDelete = data.find((user) => user.id === id);
+    res.status(200).json(userToDelete);
+  } else if (req.method === "PUT") {
+    let id = req.body.id;
     let name = req.body.name;
     let age = req.body.age;
 
@@ -24,7 +32,11 @@ export default function users(req, res) {
       name: name,
       age: age,
     };
-    data.push(user);
-    res.status(201).json(user);
+
+    let indexToUpdate = data.findIndex((user) => user.id === id);
+    data.splice(indexToUpdate, 1, user);
+
+    let userToUpdate = data.find((user) => user.id === id);
+    res.status(200).json(userToUpdate);
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import Head from "next/head";
 
 export default function Home() {
   const [name, setName] = useState("");
-  const [age, setAge] = useState(null);
+  const [age, setAge] = useState("");
+  const [id, setId] = useState("");
   const [user, setUser] = useState([]);
   const getUsers = async () => {
     let response = await fetch("/api/users");
@@ -17,11 +18,53 @@ export default function Home() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
+        id: Math.round(Math.random() * 100),
         name,
         age,
       }),
+    }).then(() => {
+      getUsers();
+      setName("");
+      setAge("");
     });
   };
+
+  const populateFields = (data) => {
+    setName(data.name);
+    setAge(data.age);
+    setId(data.id);
+  };
+
+  const updateUser = () => {
+    fetch("/api/users", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id,
+        name,
+        age,
+      }),
+    }).then(() => {
+      getUsers();
+    });
+  };
+
+  const deleteUser = (id) => {
+    fetch("/api/users", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id,
+      }),
+    }).then(() => {
+      getUsers();
+    });
+  };
+
   return (
     <>
       <Head>
@@ -34,20 +77,29 @@ export default function Home() {
         <input
           name="name"
           placeholder="Enter your Name"
+          value={name}
           onChange={(event) => setName(event.target.value)}
         />
         <input
           name="age"
           placeholder="Enter your Age"
-          onChange={(event) => setAge(Number(event.target.value))}
+          value={age}
+          onChange={(event) => setAge(event.target.value)}
         />
         <button onClick={postUsers}>Post User</button>
         <button onClick={getUsers}>Get Users</button>
+        <button onClick={updateUser}>Update User</button>
 
         {user.map((data) => {
           return (
-            <div>
+            <div key={data.id}>
               {data.name} is {data.age} years old!
+              <button onClick={() => deleteUser(data.id)}>
+                Delete this user
+              </button>
+              <button onClick={() => populateFields(data)}>
+                Populate this User
+              </button>
             </div>
           );
         })}
